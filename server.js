f78b318cdbdac34f66344834cd3e5355c89e5164
Youssef Aaouam <youssefaaouam@iMac-de-Youssef.local>
2026-04-04 13:50:32 +0200

    Fix: Point server to public_html and copy contents after build

--- server.js
+++ server.js
@@ -3,33 +3,9 @@ const path = require('path')
 const fs = require('fs')
 const app = express()
 
-// Try multiple possible locations for the out/ folder
-const possibleDirs = [
-  path.join(__dirname, 'out'),
-  path.join(__dirname, '../out'),
-  path.join(__dirname, 'public_html'),
-  path.join(__dirname, '../public_html'),
-  path.join(process.cwd(), 'out'),
-]
-
-let STATIC_DIR = null
-for (const dir of possibleDirs) {
-  if (fs.existsSync(dir)) {
-    STATIC_DIR = dir
-    console.log('Serving static files from:', dir)
-    break
-  }
-}
-
-if (!STATIC_DIR) {
-  console.error('Could not find static files directory!')
-  console.log('__dirname:', __dirname)
-  console.log('process.cwd():', process.cwd())
-  console.log('Directories checked:', possibleDirs)
-}
+const STATIC_DIR = '/home/u212839088/domains/nurturecalc.com/public_html'
 
 app.use((req, res, next) => {
-  if (!STATIC_DIR) return next()
   const urlPath = req.path.endsWith('/') ? req.path : req.path + '/'
   const indexPath = path.join(STATIC_DIR, urlPath, 'index.html')
   if (fs.existsSync(indexPath)) {
@@ -42,13 +18,7 @@ app.use((req, res, next) => {
   next()
 })
 
-if (STATIC_DIR) {
-  app.use(express.static(STATIC_DIR))
-}
-
-app.get('*', (req, res) => {
-  res.status(404).send('Static directory not found. Check runtime logs.')
-})
+app.use(express.static(STATIC_DIR))
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log('Server running on port ' + port))
