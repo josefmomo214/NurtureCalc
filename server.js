9c47c281813e986244908f2c04fb95a7701d8e34
Youssef Aaouam <youssefaaouam@iMac-de-Youssef.local>
2026-04-04 13:41:36 +0200

    Update server.js to debug static dir paths and add postbuild log

--- server.js
+++ server.js
@@ -3,9 +3,33 @@ const path = require('path')
 const fs = require('fs')
 const app = express()
 
-const STATIC_DIR = path.join(__dirname, 'out')
+// Try multiple possible locations for the out/ folder
+const possibleDirs = [
+  path.join(__dirname, 'out'),
+  path.join(__dirname, '../out'),
+  path.join(__dirname, 'public_html'),
+  path.join(__dirname, '../public_html'),
+  path.join(process.cwd(), 'out'),
+]
+
+let STATIC_DIR = null
+for (const dir of possibleDirs) {
+  if (fs.existsSync(dir)) {
+    STATIC_DIR = dir
+    console.log('Serving static files from:', dir)
+    break
+  }
+}
+
+if (!STATIC_DIR) {
+  console.error('Could not find static files directory!')
+  console.log('__dirname:', __dirname)
+  console.log('process.cwd():', process.cwd())
+  console.log('Directories checked:', possibleDirs)
+}
 
 app.use((req, res, next) => {
+  if (!STATIC_DIR) return next()
   const urlPath = req.path.endsWith('/') ? req.path : req.path + '/'
   const indexPath = path.join(STATIC_DIR, urlPath, 'index.html')
   if (fs.existsSync(indexPath)) {
@@ -18,7 +42,13 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(express.static(STATIC_DIR))
+if (STATIC_DIR) {
+  app.use(express.static(STATIC_DIR))
+}
+
+app.get('*', (req, res) => {
+  res.status(404).send('Static directory not found. Check runtime logs.')
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log('Server running on port ' + port))
